0598ff6f0940d21ab4ddc0daf648d4b9d2961579
vikrantmane7781 <nishantmane94@gmail.com>
2024-02-21 13:00:02 +0530

    70% changes done in chat screen

--- src/views/account-settings/TabAccount.js
+++ src/views/account-settings/TabAccount.js
@@ -20,40 +20,7 @@ import TextField from '@mui/material/TextField'
 import Avatar from '@mui/material/Avatar';
 import PersonIcon from 'mdi-material-ui/FaceAgent';
 import RobotIcon from 'mdi-material-ui/Robot';
-import Typist from 'react-typist';
-
-
-
-const ImgStyled = styled('img')(({ theme }) => ({
-  width: 120,
-  height: 120,
-  marginRight: theme.spacing(6.25),
-  borderRadius: theme.shape.borderRadius
-}))
-
-const ButtonStyled = styled(Button)(({ theme }) => ({
-  [theme.breakpoints.down('sm')]: {
-    width: '100%',
-    textAlign: 'center'
-  }
-}))
-
-const ResetButtonStyled = styled(Button)(({ theme }) => ({
-  marginLeft: theme.spacing(4.5),
-  [theme.breakpoints.down('sm')]: {
-    width: '100%',
-    marginLeft: 0,
-    textAlign: 'center',
-    marginTop: theme.spacing(4)
-  }
-}))
-const StickyHeader = styled(CardHeader)({
-  position: 'sticky',
-  top: 0,
-  zIndex: 1000, // Adjust the z-index as needed
- 
-});
-
+import TextareaAutosize from '@mui/material/TextareaAutosize';
 
 
 
@@ -87,52 +54,139 @@ const ChatInput = ({ onSubmit }) => {
           padding: '8px',
           borderTop: '1px solid #ccc',
           zIndex: '1',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center', // Center horizontally
         }}
       >
-       <TextField
-      type="text"
-      placeholder="Type your message..."
-      value={message}
-      onChange={handleChange}
-      fullWidth
-      variant="outlined"
-      InputProps={{
-        endAdornment: (
-          <SendIcon onClick={handleSubmit} sx={{ fontSize: '6vh' }} style={{ cursor: 'pointer'}}/>
-        ),
-      }}
-      onKeyDown={handleKeyDown}
-    />
+     <div style={{ 
+      position: 'relative',
+       flex: '1', 
+       marginRight: '8px',
+       border: '1px solid #ccc', // Add border
+       borderRadius: '4px', // Add border radius
+       maxWidth: 'calc(100% - 60px)', // Adjust the width here
+         }}>
+        <TextareaAutosize
+          rows={1} // Set initial number of rows
+          maxRows={6} // Maximum number of rows before scrolling
+          placeholder="Type your message..."
+          value={message}
+          onChange={handleChange}
+          style={{
+            width: '96%', // Full width of parent container
+            resize: 'none',
+            overflowY: 'auto',
+            fontSize: '16px',
+            border: 'none', // Remove border
+            padding: '8px', // Add padding
+            boxSizing: 'border-box', // Ensure padding doesn't affect width
+            outline: 'none', // Remove outline on focus
+          }}
+          onKeyDown={handleKeyDown}
+        />
+        <SendIcon
+          onClick={handleSubmit}
+          sx={{ position: 'absolute', right: '8px', top: '50%', transform: 'translateY(-50%)', fontSize: '35px', cursor: 'pointer' }}
+        />
+      </div>
       </Box>
     );
   };
 
-  const ChatMessage = ({ sender, text ,isNew}) => {
-    const renderIcon = () => {
-      if (sender === 'You') {
-        return <Avatar><PersonIcon /></Avatar>;
-      } else if (sender != 'You') {
-        return <Avatar><RobotIcon /></Avatar>;
-      } else {
-        return null;
-      }
-    };
+  const ChatMessage = ({ sender, text ,isNew,isMostRecentBotMessage}) => {
+
+
+    const [typingText, setTypingText] = useState('');
+
+    useEffect(() => {
+      if (isNew && isMostRecentBotMessage) {
+        const typeWriter = () => {
+          let i = 0;
+          const speed = 10; // typing speed in milliseconds
+          const message = text;
   
+          const typingInterval = setInterval(() => {
+            if (i < message.length) {
+              setTypingText(prevText => prevText + message.charAt(i));
+              i++;
+            } else {
+              clearInterval(typingInterval);
+            }
+          }, speed);
+        };
+  
+        // Start typing effect
+        typeWriter();
+      }
+    }, [isNew, isMostRecentBotMessage, text]);
+  
+
+          const renderIcon = () => {
+            if (sender === 'You') {
+              return <Avatar alt='John Doe' src='/images/avatars/3.png' sx={{ width: '2.5rem', height: '2.5rem',borderRadius: '5px' }} />;
+            } else if (sender != 'You') {
+              return <Avatar alt='John Doe' src='/images/avatars/bot.png' sx={{ width: '2.5rem', height: '2.5rem',borderRadius: '5px' }} />;
+            } else {
+              return null;
+            }
+          };
+
     return (
-      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '22px' }}>
-        {renderIcon()}
-        {sender != 'You' ? (
-        isNew ? (
-          <Typist>
-            <span>{text}</span>
-          </Typist>
+      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '2vh',alignItems: 'flex-start',  }}>
+      {renderIcon()}
+      <div style={{ width: '100%',boxShadow: '0 2px 4px rgba(0,0,0,0.1)', borderRadius: '5px', border: '1px solid rgba(0,0,0,0.1)',marginLeft:'2%' }}>
+        {sender !== 'You' ? (
+          isMostRecentBotMessage && isNew ? (
+            <div className="typing-effect">
+              <TextareaAutosize
+                value={typingText}
+                style={{
+                  width: '100%', // Full width of parent container
+                  resize: 'none',
+                  overflowY: 'auto',
+                  fontSize: '16px',
+                  border: 'none', // Remove border
+                  padding: '8px', // Add padding
+                  boxSizing: 'border-box', // Ensure padding doesn't affect width
+                  outline: 'none', // Remove outline on focus
+                }}
+              />
+            </div>
+          ) : (
+            <TextareaAutosize
+              value={text}
+              readOnly
+              style={{
+                width: '100%', // Full width of parent container
+                resize: 'none',
+                overflowY: 'auto',
+                fontSize: '16px',
+                border: 'none', // Remove border
+                padding: '8px', // Add padding
+                boxSizing: 'border-box', // Ensure padding doesn't affect width
+                outline: 'none', // Remove outline on focus
+              }}
+            />
+          )
         ) : (
-          <span>{text}</span>
-        )
-      ) : (
-        <span>{text}</span>
-      )}
+          <TextareaAutosize
+            value={text}
+            readOnly
+            style={{
+              width: '100%', // Full width of parent container
+              resize: 'none',
+              overflowY: 'auto',
+              fontSize: '16px',
+              border: 'none', // Remove border
+              padding: '8px', // Add padding
+              boxSizing: 'border-box', // Ensure padding doesn't affect width
+              outline: 'none', // Remove outline on focus
+            }}
+          />
+        )}
       </div>
+    </div>
     );
   };
 
@@ -143,6 +197,8 @@ const TabAccount = () => {
   const [chatMessages, setChatMessages] = useState([]);
   const [isUserTyping, setIsUserTyping] = useState(false);
   const chatContainerRef = useRef(null);
+  const [mostRecentBotMessageIndex, setMostRecentBotMessageIndex] = useState(-1);
+
 
   const scrollToBottom = () => {
     chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
@@ -172,34 +228,52 @@ const TabAccount = () => {
   localStorage.setItem('chatMessages', JSON.stringify(updatedMessages));
 
   setTimeout(() => {
-    const botResponse = { sender: 'Bot', text: `Response to: ${message}`, isNew: true };
+    const botResponse = { sender: 'Bot', text: `${message}`, isNew: true };
     const updatedMessagesWithBotResponse = [...updatedMessages, botResponse];
     setChatMessages(updatedMessagesWithBotResponse);
+    setMostRecentBotMessageIndex(updatedMessagesWithBotResponse.length - 1);
     setIsUserTyping(false);
 
-    // Save updated chat messages with bot response to local storage
     localStorage.setItem('chatMessages', JSON.stringify(updatedMessagesWithBotResponse));
   }, 1000);
   };
 
-
+  useEffect(() => {
+    window.addEventListener('beforeunload', function() {
+      localStorage.clear();
+    });
+  
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('beforeunload', function() {
+        localStorage.clear();
+      });
+    };
+  }, []);
 
 
   return (
     <>
         <CardContent sx={{ height: 'calc(100vh - 200px)' }}>
+        <Typography>Active Project:</Typography>
         <Box 
         id="chat-container" 
         ref={chatContainerRef}
-        sx={{ height: 'calc(100vh - 200px)',width: '100%', overflowY: 'auto', padding: '10px',mb:'22px'}}>
+        sx={{ height: 'calc(100vh - 200px)',width: '100%', overflowY: 'auto', padding: '10px',mb:'22px',mt:'20px'}}>
 
-              <div style={{ width: '200px', wordWrap: 'break-word' }}>
+              <div style={{ width: '100%', wordWrap: 'break-word' }}>
               {chatMessages.map((message, index) => (
-                        <ChatMessage key={index} sender={message.sender} text={message.text} />
+                        <ChatMessage
+                        key={index}
+                        sender={message.sender}
+                        text={message.text}
+                        isNew={message.isNew}
+                        isMostRecentBotMessage={index === mostRecentBotMessageIndex}
+                      />
                       ))}
                   {isUserTyping && (
                 <Typography variant="body1" style={{ marginTop: '10px' }}>
-                  You are typing...
+                  Waiting for reply...
                 </Typography>
               )}
             </div>
